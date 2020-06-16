bf89cc1b4bb56cc91f520010fc632e4d29fc4ac3
myDiamondsDancing <66516985+dmitriyKolmogorov@users.noreply.github.com>
2020-06-16 13:47:28 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name="asm",
+      version="0.0.1",
+      author="Kolmogorov Dmitriy",
+      author_email="kolmogorov.dmitro@gmail.com",
+      description="Run assembler source using Python"
+      url="https://github.com/dmitriyKolmogorov/PyXAsm",
+      packages=find_packages())
