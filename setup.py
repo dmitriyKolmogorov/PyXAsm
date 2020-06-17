dc99a17119d04481fe29e1d19bf575300d0f738a
myDiamondsDancing <66516985+dmitriyKolmogorov@users.noreply.github.com>
2020-06-17 14:40:26 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ setup(name="asm",
       version="0.0.1",
       author="Kolmogorov Dmitriy",
       author_email="kolmogorov.dmitro@gmail.com",
-      description="Run assembler source using Python"
+      description="Run assembler source using Python",
       url="https://github.com/dmitriyKolmogorov/PyXAsm",
       packages=find_packages())
